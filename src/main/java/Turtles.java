838f3a3a27da18346cd42f3da07450310a9ee651
eric <ericarterbridge@gmail.com>
2022-08-15 12:13:10 -0400

    working on gcd method

--- src/main/java/Turtles.java
+++ src/main/java/Turtles.java
@@ -1,7 +1,21 @@
-package PACKAGE_NAME;
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class Turtles {
+
+    public int factorial(int n) {
+
+        if (n == 1){
+            return n;
+        }
+
+        return n * factorial(n - 1);
+    }
+
+    public int gcd(int p, int q) {
+
+        if (q == 0) return p;
+        else return gcd(q, p%q);
+    }
 }
--- src/test/java/TurtlesTest.java
+++ src/test/java/TurtlesTest.java
@@ -1,8 +1,33 @@
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class TurtlesTest {
+
+    Turtles turtles = new Turtles();
+
+    @Test
+    public void smallFactorialTest(){
+        int expected = 120;
+        int actual = turtles.factorial(5);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void largeFactorialTest(){
+        int expected = 3628800;
+        int actual = turtles.factorial(10);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void oneFactorialTest(){
+        int expected = 1;
+        int actual = turtles.factorial(1);
+        Assert.assertEquals(expected, actual);
+    }
 
 }
